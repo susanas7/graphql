78a386a185286e1aace4b0b6c00c30edca712a6b
Susana S <susana.s7@hotmail.com>
2024-05-30 12:55:40 -0500

    Lab 4

--- src/graphql/resolvers/persona.ts
+++ src/graphql/resolvers/persona.ts
@@ -5,6 +5,39 @@ const personaResolver: IResolvers = {
     Query: {
         obtenerPersona(){
             return PersonaDataSource;
+        },
+        obtenerPersonaPorNombre: (parent, { nombre }) => {
+            return PersonaDataSource.filter(PersonaDataSource => PersonaDataSource.nombre.toLowerCase().includes(nombre.toLowerCase()));
+        }
+    },
+    Mutation: {
+        crearPersona: (parent, { input }) => {
+            const newPerson = {
+                id: String(PersonaDataSource.length + 1),
+                ...input
+            };
+            PersonaDataSource.push(newPerson);
+            return newPerson;
+        },
+        actualizarPersona: (parent, { id, input }) => {
+            const personIndex = PersonaDataSource.findIndex(person => person.id === id);
+            if (personIndex !== -1) {
+                const updatedPersona = {
+                    id,
+                    ...input
+                };
+                PersonaDataSource[personIndex] = updatedPersona;
+                return updatedPersona;
+            }
+            throw new Error("Persona not found");
+        },
+        eliminarPersona: (parent, { id }) => {
+            const personIndex = PersonaDataSource.findIndex(person => person.id === id);
+            if (personIndex !== -1) {
+                PersonaDataSource.splice(personIndex, 1);
+                return true;
+            }
+            throw new Error("Persona not found");
         }
     }
 }
